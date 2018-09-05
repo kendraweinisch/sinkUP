87067e13449461ed3fcf0d825db536e0c093b71a
Kendra Weinisch <kendraweinisch@gmail.com>
2018-09-05 16:50:26 -0700

    Login page connection

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -5,7 +5,6 @@ const clientId = '1086638400321-96p0sokdamjj9ape2gfja03dj87vajir.apps.googleuser
 // import GoogleLogin, { GoogleLogout } from '../dist/google-login'
 
 const success = response => {
-<<<<<<< HEAD
   console.log("successful login")
   const {profileObj} = response;
   console.log(profileObj);
@@ -30,17 +29,6 @@ const success = response => {
   .then(response => console.log('Success:', JSON.stringify(response)))
   .catch(error => console.error('Error:', error));
   // find a way to store googleId in state
-=======
-  console.log(response.profileObj)
-  let user = {
-    googleID: response.profileObj.googleId,
-    userName: response.profileObj.name,
-    userEmail: response.profileObj.email,
-    userPhoto: response.profileObj.imageUrl,
-  }
-  console.log(user)
-
->>>>>>> d8f11a9349628815bf88d8ca2fb23ac4320f0ee9
 };
 
 const error = response => {
